b84e9e6d46c00f6a837c85eae9adb66f73df4ac1
Pranay Pathole <38600100+PPathole@users.noreply.github.com>
2022-07-08 18:49:54 +0530

    Update CRUD.sql

--- CRUD.sql
+++ CRUD.sql
@@ -62,6 +62,38 @@ DELETE FROM job
 WHERE job_id = 2;
 
 
+/*ALTER TABLE information
+RENAME TO new_information;*/
 
+/*ALTER TABLE new_information
+RENAME COLUMN person TO people;*/
+
+/*ALTER TABLE new_information
+ALTER COLUMN people DROP NOT NULL;*/
+
+/*ALTER TABLE new_information
+DROP COLUMN people;*/
+
+/*CREATE TABLE employees(
+    emp_id SERIAL PRIMARY KEY,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    birthdate DATE CHECK (birthdate > '1900-01-01'),
+    hire_date DATE CHECK (hire_date > birthdate),
+    salary INTEGER CHECK (salary > 0)
+)*/
+
+INSERT INTO employees (
+    first_name,
+    last_name,
+    birthdate,
+    hire_date,
+    salary
+)VALUES
+('Sammy',
+ 'Smith',
+ '1899-11-03',
+ '2010-01-01',
+ 100)
 
 
